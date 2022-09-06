5001b666e1f6c4848769113a7001f87dd4d33d81
Nishant Bhatnagar <nishant.bhatnagar@firsthive.com>
2022-09-06 16:44:49 +0530

    user dashboard page updated

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.smart.controller;
 
+import com.smart.model.User;
+import com.smart.service.IUserRepositoryImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    IUserRepositoryImplementation iUserRepositoryImplementation;
     @RequestMapping("/index")
-    public ModelAndView dashborad() {
+    public ModelAndView dashborad(Principal principal) {
+        String userName = principal.getName();
+        User user = iUserRepositoryImplementation.getUserLogin(userName);
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user",user);
         modelAndView.setViewName("normal/user_dashboard");
         return modelAndView;
     }
