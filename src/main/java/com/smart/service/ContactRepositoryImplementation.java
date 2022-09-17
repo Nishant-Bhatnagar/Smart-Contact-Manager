479c9a28e3219e49680036e103c71bd36c95e501
nishant-debug <nishant010599@gmail.com>
2022-09-18 00:02:53 +0530

    image storing in DB and File

--- src/main/java/com/smart/service/ContactRepositoryImplementation.java
+++ src/main/java/com/smart/service/ContactRepositoryImplementation.java
@@ -3,8 +3,16 @@ package com.smart.service;
 import com.smart.model.Contact;
 import com.smart.repository.IContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
@@ -17,6 +25,7 @@ public class ContactRepositoryImplementation implements IContactRepositoryImplem
         System.out.println("Insode");
         return iContactRepository.findAll();
     }
+
 
 
 }
--- src/main/java/com/smart/service/IContactRepositoryImplementation.java
+++ src/main/java/com/smart/service/IContactRepositoryImplementation.java
@@ -1,12 +1,14 @@
 package com.smart.service;
 
 import com.smart.model.Contact;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface IContactRepositoryImplementation {
 
     List<Contact> getContact();
+
 
 
 }
