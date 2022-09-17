3afb6a3461365606e51a20339e59f1738302f288
nishant-debug <nishant010599@gmail.com>
2022-09-17 20:19:16 +0530

    Add contact page form

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.smart.controller;
 
+import com.smart.model.Contact;
 import com.smart.model.User;
 import com.smart.service.IUserRepositoryImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,13 +17,29 @@ import java.security.Principal;
 public class UserController {
     @Autowired
     IUserRepositoryImplementation iUserRepositoryImplementation;
-    @RequestMapping("/index")
-    public ModelAndView dashborad(Principal principal) {
+    public ModelAndView addCommonData(Principal principal)
+    {
         String userName = principal.getName();
         User user = iUserRepositoryImplementation.getUserLogin(userName);
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("user",user);
-        modelAndView.setViewName("normal/user_dashboard");
         return modelAndView;
+    }
+    @GetMapping("/index")
+    public ModelAndView dashBoard(Principal principal) {
+        ModelAndView mv = addCommonData(principal);
+        mv.addObject("title","User DashBoard");
+        mv.setViewName("normal/user_dashboard");
+        return mv;
+    }
+
+    @GetMapping("/add-contact")
+    public ModelAndView openAddContactForm(Principal principal)
+    {
+        ModelAndView mv = addCommonData(principal);
+         mv.addObject("title","Add Contact");
+         mv.addObject("contact",new Contact());
+        mv.setViewName("normal/add_contact_form");
+        return mv;
     }
 }
